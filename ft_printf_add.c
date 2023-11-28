69a4f76ad39c8782748ed99ea0c698120603b5bc
ppenquinn <penguin32439@outlook.co.th>
2023-11-28 11:32:10 +0700

    nappalav

--- ft_printf_add.c
+++ ft_printf_add.c
@@ -6,23 +6,26 @@
 /*   By: nappalav <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 17:16:09 by nappalav          #+#    #+#             */
-/*   Updated: 2023/11/26 17:27:09 by nappalav         ###   ########.fr       */
+/*   Updated: 2023/11/26 23:57:40 by nappalav         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	ft_printf_lpos(unsigned long nbr, char *base, unsigned int div)
+static int	ft_printf_lpos(unsigned long nbr, char *base, unsigned int div)
 {
 	char			ans;
 
 	if (nbr >= div)
 	{
-		ft_printf_lpos(nbr / div, base, div);
+		if (ft_printf_lpos(nbr / div, base, div) == -1)
+			return (-1);
 		nbr %= div;
 	}
 	ans = base[nbr];
-	write(1, &ans, 1);
+	if (write(1, &ans, 1) == -1)
+		return (-1);
+	return (0);
 }
 
 int	ft_printf_p(unsigned long nbr, char *base)
@@ -34,7 +37,8 @@ int	ft_printf_p(unsigned long nbr, char *base)
 	length = 0;
 	temp = nbr;
 	div = ft_strlen(base);
-	write(1, "0x", 2);
+	if (write(1, "0x", 2) == -1)
+		return (-1);
 	if (temp == 0)
 		length++;
 	while (temp > 0)
@@ -42,12 +46,7 @@ int	ft_printf_p(unsigned long nbr, char *base)
 		temp /= div;
 		length++;
 	}
-	ft_printf_lpos(nbr, base, div);
+	if (ft_printf_lpos(nbr, base, div) == -1)
+		return (-1);
 	return (2 + length);
 }
-
-// int	ft_printf_p(unsigned long nbr, char *base)
-// {
-// 	write(1, "0x", 2);
-// 	return (2 + ft_printf_ul(nbr, base));
-// }
--- test.c
+++
@@ -1,8 +0,0 @@
-#include "ft_printf.h"
-
-int main (void)
-{
-	printf("%d\n",ft_printf(" %p %p \n", LONG_MIN, LONG_MAX));
-	printf("%d\n", printf(" %p %p \n", LONG_MIN, LONG_MAX));
-	//printf(" %x %x %p %p\n", LONG_MIN + 1, LONG_MAX, LONG_MIN, LONG_MAX);
-}
